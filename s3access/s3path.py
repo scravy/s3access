4cdd15dffa53c0043e172a9746786678936488c1
Julian Fleischer <tirednesscankill@warhog.net>
2021-05-01 16:45:28 +0200

    Add methods and tests for S3Path

--- s3access/s3path.py
+++ s3access/s3path.py
@@ -1,20 +1,11 @@
 from __future__ import annotations
 
+import re
 from typing import Optional, Dict
 
 
 class S3Path:
-
-    def __init__(self, bucket_or_url, key: Optional[str] = None):
-        if isinstance(bucket_or_url, S3Path):
-            self._bucket = bucket_or_url._bucket
-            self._key = bucket_or_url._key
-        elif bucket_or_url and key is not None:
-            self._bucket = bucket_or_url
-            self._key = key
-        else:
-            self._bucket = ''
-            self._key = ''
+    _path_pattern = re.compile(r"([sS]3[aAnN]?:/*)*(?P<bucket>[^/:]+)(/(?P<key>.+)?)?")
 
     @property
     def bucket(self) -> str:
@@ -26,16 +17,51 @@ class S3Path:
 
     @property
     def params(self) -> Dict[str, str]:
-        return {}
-
-    def with_key(self, new_key) -> S3Path:
-        pass
+        return self._params
 
     def with_bucket(self, new_bucket) -> S3Path:
-        pass
+        return S3Path(new_bucket, self._key)
+
+    def with_key(self, new_key) -> S3Path:
+        return S3Path(self._bucket, new_key)
 
     def with_params(self, **kwargs) -> S3Path:
-        pass
+        components = []
+        applied = set()
+        for component in self._key.split('/'):
+            parts = component.split('=', 1)
+            if len(parts) == 2:
+                key, value = parts
+                if key in kwargs:
+                    applied.add(key)
+                    components.append(f"{key}={kwargs[key]}")
+                    continue
+            components.append(component)
+        for key, value in kwargs.items():
+            if key not in applied:
+                components.append(f"{key}={value}")
+        return self.with_key('/'.join(components))
+
+    def __init__(self, bucket_or_url, key: Optional[str] = None):
+        if isinstance(bucket_or_url, S3Path):
+            self._bucket = bucket_or_url._bucket
+            self._key = bucket_or_url._key
+        elif bucket_or_url and key is not None:
+            self._bucket = bucket_or_url
+            self._key = key
+        else:
+            result = self._path_pattern.fullmatch(bucket_or_url)
+            if not result:
+                raise ValueError
+            self._bucket = result.group('bucket') or ''
+            self._key = result.group('key') or ''
+        self._key = self._key.rstrip('/')
+        self._params = {}
+        for component in self._key.split('/'):
+            parts = component.split('=', 1)
+            if len(parts) == 2:
+                key, value = parts
+                self._params[key] = value
 
     def __getitem__(self, item):
         return self.params[item]
@@ -44,4 +70,20 @@ class S3Path:
         return item in self.params
 
     def __truediv__(self, key) -> S3Path:
-        pass
+        return self.with_key(self.key + '/' + key)
+
+    def __eq__(self, other):
+        if isinstance(other, S3Path):
+            return self._bucket == other._bucket and self._key == other._key
+        if isinstance(other, str):
+            return self == S3Path(other)
+        raise NotImplementedError
+
+    def __hash__(self):
+        return hash((self._bucket, self._key))
+
+    def __str__(self):
+        return f's3://{self._bucket}/{self._key}'
+
+    def __repr__(self):
+        return f'S3Path({str(self)})'
---
+++ tests/test_s3path.py
@@ -0,0 +1,50 @@
+import unittest
+
+from s3access.s3path import S3Path
+
+
+class S3PathTestCase(unittest.TestCase):
+    def test_s3path(self):
+        self.assertEqual(
+            S3Path('s3://bucket/key'),
+            S3Path('s3a://bucket/key'),
+        )
+        self.assertEqual(
+            S3Path('s3://bucket/key'),
+            S3Path('s3://bucket/key/'),
+        )
+
+    def test_params(self):
+        p = S3Path('s3://bucket/foo=bar')
+        self.assertEqual({
+            'foo': 'bar',
+        }, p.params)
+
+    def test_with_params(self):
+        p = S3Path('s3://bucket/foo=bar')
+        p2 = p.with_params(foo='qux')
+        self.assertEqual({
+            'foo': 'qux',
+        }, p2.params)
+        p3 = p.with_params(qux='quuz', klm='pqq')
+        self.assertEqual({
+            'foo': 'bar',
+            'qux': 'quuz',
+            'klm': 'pqq',
+        }, p3.params)
+        self.assertEqual(
+            S3Path('bucket/foo=bar/qux=quuz/klm=pqq'),
+            p3,
+        )
+
+    def test_append(self):
+        p = S3Path('s3://bucket/foo=bar')
+        p /= 'hey=yea'
+        self.assertEqual(
+            p,
+            S3Path('s3n://bucket/foo=bar/hey=yea')
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
