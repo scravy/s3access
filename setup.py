584d18cbd2b022b357ff3ae9e198245ce1bfe49c
Julian Fleischer <tirednesscankill@warhog.net>
2021-05-01 15:56:04 +0200

    Skeleton

---
+++ setup.py
@@ -0,0 +1,38 @@
+import re
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    start_result = re.search(r"<!-- START doctoc [^>]+-->", long_description)
+    if start_result:
+        pos = start_result.span(0)[0]
+        before = long_description[:pos]
+        remaining = long_description[pos:]
+        end_result = re.search(r"<!-- END doctoc [^>]+-->", remaining)
+        end_pos = end_result.span(0)[1]
+        after = remaining[end_pos:]
+        long_description = before + after
+
+__pkginfo__ = {}
+with open("s3access/__pkginfo__.py") as fh:
+    exec(fh.read(), __pkginfo__)
+
+setuptools.setup(
+    name="s3access",
+    version=__pkginfo__['__version__'],
+    author="Julian Fleischer",
+    author_email="tirednesscankill@warhog.net",
+    description="Access Parquet files in S3 via S3 Select",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scravy/s3access",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
